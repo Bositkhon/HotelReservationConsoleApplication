fbfa640f9c4ea9f7c0962a1235d5bcefe2ef893f
Bositkhon <bositkhon.sh@gmail.com>
2018-11-16 23:44:53 +0500

    Improved some bugs #1

--- src/com/firstapp/Main.java
+++ src/com/firstapp/Main.java
@@ -1,7 +1,6 @@
 package com.firstapp;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
 
@@ -26,7 +25,6 @@ public class Main {
 
 
             command = sc.nextInt();
-
             switch (command){
                 case 1:{
                     myHotel.viewAllRooms();
@@ -37,17 +35,32 @@ public class Main {
                     break;
                 }
                 case 3:{
-                    System.out.println("Please enter the room number from (1 to " + myHotel.TOTAL_NUMBER_OF_ROOMS + ")");
+                    System.out.println("Please enter the room number from (1 to " + Hotel.TOTAL_NUMBER_OF_ROOMS + ")");
                     int roomID = sc.nextInt();
                     System.out.println("Please enter your name");
-                    String owner = sc.next();;
+                    String owner = sc.next();
                     myHotel.bookRoom(roomID, owner);
+                    break;
                 }
+
+                case 4:{
+                    System.out.println("If there is a bug please feel free to send us a message clearly indicating the incompleteness of our application");
+                    String feedback;
+                    feedback = sc.nextLine();
+                    if(feedback.isEmpty()){
+                        System.out.println("Are you sure you want to leave the message empty?");
+                    }else{
+                        System.out.println("Thank you for your feedback!");
+                    }
+                    break;
+                }
+
                 default:{
                     System.out.println("You entered an invalid data");
                     System.out.println("Please try again!");
                     break;
                 }
+
             }
         }
     }
